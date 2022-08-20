50d7306c5f50df1d037f2d3aa8d21c8469b18aed
tonygiang <tony_s_giang@yahoo.com>
2022-08-20 16:16:14 +0700

    Project push.

---
+++ README.md
@@ -0,0 +1,63 @@
+﻿# CLSS.ExtensionMethods.Reference.Action.Once
+
+### Problem
+
+Registering a method that will execute only once upon invocation of an event is a common operation in the observer pattern. In languages such as JavaScript, this is done in one line by specifying an options property:
+
+```
+element.addEventListener("click", () => { ... }, { once: true });
+```
+
+In C#, it is done like this:
+
+```
+Action OnEvent = delegate { }
+
+Action wrapperEvent = null;
+wrapperEvent = () =>
+{
+  OnEvent -= wrapperEvent;
+  // Actions that will only execute once
+}
+OnEvent += wrapperEvent;
+```
+
+This syntax can use some improvements so that it wouldn't take a minimum of 7 lines of code.
+
+However, in the lack of native language support for registering a method as an one-off invocation, providing an 1-line syntax for this operation via extension methods poses some challenges. Delegates are immutable, and the `+=` & `-=` operators will in fact replace the reference on the left-hand side with a new reference. Since source argument of an extension method is always passed by value, calling `+=` on a copied reference of a delegate will only replace that copied reference while the original delegate remains unreplaced. However, passing the reference encapsuling that delegate will preserve the delegate's reference. This calls for a general-purpose encapsulating reference type to make this syntax work.
+
+### Solution
+
+By utilising the CLSS type [`Reference`](https://www.nuget.org/packages/CLSS.Types.Reference) - a general-purpose reference-type wrapper for a single value of any type, this package is able to provide the `Once` extension method to a delegate encapsulated in a `CLSS.Reference`. The one-off method registration syntax can now be done in as short as one line.
+
+```
+using CLSS;
+
+Reference<Action> OnEvent = new Reference<Action>(delegate { });
+
+OnEvent.Once(() => { ... });
+
+// Test invocations
+OnEvent.Value.Invoke(); // Registered method only executes on this invocation
+OnEvent.Value.Invoke();
+```
+
+`Once` only works well as long as the encapsulated delegate is used via its encapsulating `CLSS.Reference`. Creating a `CLSS.Reference` out of a raw delegate field will not result in methods being registered to the raw delegate.
+
+```
+using CLSS;
+
+Action RawEvent = delegate { };
+
+// Object.ReferenceEquals(EventRef.Value, RawEvent) is still true at this line
+var EventRef = new Reference<Action>(RawEvent);
+EventRef.Once(MethodA); // reference replaced inside EventRef
+RawEvent += MethodB; // RawEvent replaced
+
+// Test invocations
+RawEvent.Invoke(); // Does not call MethodA, will call MethodB
+EventRef.Value.Invoke(); // Will call MethodA, does not call MethodB
+EventRef.Value.Invoke(); // Does not call MethodA or MethodB
+```
+
+##### This package is a part of the [C# Language Syntactic Sugar suite](https://github.com/tonygiang/CLSS).
---
+++ Reference.Action.Once.cs
@@ -0,0 +1,542 @@
+﻿// A part of the C# Language Syntactic Sugar suite.
+
+using System;
+
+namespace CLSS
+{
+  public static partial class ReferenceActionOnce
+  {
+    /// <inheritdoc cref="Once{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}(Reference{Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}}, Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16})"/>
+    public static Reference<Action> Once(this Reference<Action> source,
+      Action listener)
+    {
+      Action wrapperAction = null;
+      wrapperAction = () =>
+      {
+        source.Value -= wrapperAction;
+        listener();
+      };
+      source.Value += wrapperAction;
+      return source;
+    }
+
+    /// <inheritdoc cref="Once{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}(Reference{Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}}, Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16})"/>
+    public static Reference<Action<T1>> Once<T1>(
+      this Reference<Action<T1>> source,
+      Action<T1> listener)
+    {
+      Action<T1> wrapperAction = null;
+      wrapperAction = (arg1) =>
+      {
+        source.Value -= wrapperAction;
+        listener(arg1);
+      };
+      source.Value += wrapperAction;
+      return source;
+    }
+
+    /// <inheritdoc cref="Once{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}(Reference{Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}}, Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16})"/>
+    public static Reference<Action<T1, T2>> Once<T1, T2>(
+      this Reference<Action<T1, T2>> source,
+      Action<T1, T2> listener)
+    {
+      Action<T1, T2> wrapperAction = null;
+      wrapperAction = (arg1,
+        arg2) =>
+      {
+        source.Value -= wrapperAction;
+        listener(arg1,
+          arg2);
+      };
+      source.Value += wrapperAction;
+      return source;
+    }
+
+    /// <inheritdoc cref="Once{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}(Reference{Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}}, Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16})"/>
+    public static Reference<Action<T1, T2, T3>> Once<T1, T2, T3>(
+      this Reference<Action<T1, T2, T3>> source,
+      Action<T1, T2, T3> listener)
+    {
+      Action<T1, T2, T3> wrapperAction = null;
+      wrapperAction = (arg1,
+        arg2,
+        arg3) =>
+      {
+        source.Value -= wrapperAction;
+        listener(arg1,
+          arg2,
+          arg3);
+      };
+      source.Value += wrapperAction;
+      return source;
+    }
+
+    /// <inheritdoc cref="Once{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}(Reference{Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}}, Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16})"/>
+    public static Reference<Action<T1, T2, T3, T4>> Once<T1, T2, T3, T4>(
+      this Reference<Action<T1, T2, T3, T4>> source,
+      Action<T1, T2, T3, T4> listener)
+    {
+      Action<T1, T2, T3, T4> wrapperAction = null;
+      wrapperAction = (arg1,
+        arg2,
+        arg3,
+        arg4) =>
+      {
+        source.Value -= wrapperAction;
+        listener(arg1,
+          arg2,
+          arg3,
+          arg4);
+      };
+      source.Value += wrapperAction;
+      return source;
+    }
+
+    /// <inheritdoc cref="Once{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}(Reference{Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}}, Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16})"/>
+    public static Reference<Action<T1, T2, T3, T4, T5>> Once<T1, T2, T3, T4, T5>(
+      this Reference<Action<T1, T2, T3, T4, T5>> source,
+      Action<T1, T2, T3, T4, T5> listener)
+    {
+      Action<T1, T2, T3, T4, T5> wrapperAction = null;
+      wrapperAction = (arg1,
+        arg2,
+        arg3,
+        arg4,
+        arg5) =>
+      {
+        source.Value -= wrapperAction;
+        listener(arg1,
+          arg2,
+          arg3,
+          arg4,
+          arg5);
+      };
+      source.Value += wrapperAction;
+      return source;
+    }
+
+    /// <inheritdoc cref="Once{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}(Reference{Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}}, Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16})"/>
+    public static Reference<Action<T1, T2, T3, T4, T5, T6>> Once<T1, T2, T3, T4, T5, T6>(
+      this Reference<Action<T1, T2, T3, T4, T5, T6>> source,
+      Action<T1, T2, T3, T4, T5, T6> listener)
+    {
+      Action<T1, T2, T3, T4, T5, T6> wrapperAction = null;
+      wrapperAction = (arg1,
+        arg2,
+        arg3,
+        arg4,
+        arg5,
+        arg6) =>
+      {
+        source.Value -= wrapperAction;
+        listener(arg1,
+          arg2,
+          arg3,
+          arg4,
+          arg5,
+          arg6);
+      };
+      source.Value += wrapperAction;
+      return source;
+    }
+
+    /// <inheritdoc cref="Once{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}(Reference{Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}}, Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16})"/>
+    public static Reference<Action<T1, T2, T3, T4, T5, T6, T7>> Once<T1, T2, T3, T4, T5, T6, T7>(
+      this Reference<Action<T1, T2, T3, T4, T5, T6, T7>> source,
+      Action<T1, T2, T3, T4, T5, T6, T7> listener)
+    {
+      Action<T1, T2, T3, T4, T5, T6, T7> wrapperAction = null;
+      wrapperAction = (arg1,
+        arg2,
+        arg3,
+        arg4,
+        arg5,
+        arg6,
+        arg7) =>
+      {
+        source.Value -= wrapperAction;
+        listener(arg1,
+          arg2,
+          arg3,
+          arg4,
+          arg5,
+          arg6,
+          arg7);
+      };
+      source.Value += wrapperAction;
+      return source;
+    }
+
+    /// <inheritdoc cref="Once{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}(Reference{Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}}, Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16})"/>
+    public static Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8>> Once<T1, T2, T3, T4, T5, T6, T7, T8>(
+      this Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8>> source,
+      Action<T1, T2, T3, T4, T5, T6, T7, T8> listener)
+    {
+      Action<T1, T2, T3, T4, T5, T6, T7, T8> wrapperAction = null;
+      wrapperAction = (arg1,
+        arg2,
+        arg3,
+        arg4,
+        arg5,
+        arg6,
+        arg7,
+        arg8) =>
+      {
+        source.Value -= wrapperAction;
+        listener(arg1,
+          arg2,
+          arg3,
+          arg4,
+          arg5,
+          arg6,
+          arg7,
+          arg8);
+      };
+      source.Value += wrapperAction;
+      return source;
+    }
+
+    /// <inheritdoc cref="Once{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}(Reference{Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}}, Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16})"/>
+    public static Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9>> Once<T1, T2, T3, T4, T5, T6, T7, T8, T9>(
+      this Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9>> source,
+      Action<T1, T2, T3, T4, T5, T6, T7, T8, T9> listener)
+    {
+      Action<T1, T2, T3, T4, T5, T6, T7, T8, T9> wrapperAction = null;
+      wrapperAction = (arg1,
+        arg2,
+        arg3,
+        arg4,
+        arg5,
+        arg6,
+        arg7,
+        arg8,
+        arg9) =>
+      {
+        source.Value -= wrapperAction;
+        listener(arg1,
+          arg2,
+          arg3,
+          arg4,
+          arg5,
+          arg6,
+          arg7,
+          arg8,
+          arg9);
+      };
+      source.Value += wrapperAction;
+      return source;
+    }
+
+    /// <inheritdoc cref="Once{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}(Reference{Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}}, Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16})"/>
+    public static Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>> Once<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(
+      this Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>> source,
+      Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> listener)
+    {
+      Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> wrapperAction = null;
+      wrapperAction = (arg1,
+        arg2,
+        arg3,
+        arg4,
+        arg5,
+        arg6,
+        arg7,
+        arg8,
+        arg9,
+        arg10) =>
+      {
+        source.Value -= wrapperAction;
+        listener(arg1,
+          arg2,
+          arg3,
+          arg4,
+          arg5,
+          arg6,
+          arg7,
+          arg8,
+          arg9,
+          arg10);
+      };
+      source.Value += wrapperAction;
+      return source;
+    }
+
+    /// <inheritdoc cref="Once{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}(Reference{Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}}, Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16})"/>
+    public static Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>> Once<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(
+      this Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>> source,
+      Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> listener)
+    {
+      Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> wrapperAction = null;
+      wrapperAction = (arg1,
+        arg2,
+        arg3,
+        arg4,
+        arg5,
+        arg6,
+        arg7,
+        arg8,
+        arg9,
+        arg10,
+        arg11) =>
+      {
+        source.Value -= wrapperAction;
+        listener(arg1,
+          arg2,
+          arg3,
+          arg4,
+          arg5,
+          arg6,
+          arg7,
+          arg8,
+          arg9,
+          arg10,
+          arg11);
+      };
+      source.Value += wrapperAction;
+      return source;
+    }
+
+    /// <inheritdoc cref="Once{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}(Reference{Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}}, Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16})"/>
+    public static Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>> Once<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>(
+      this Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>> source,
+      Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> listener)
+    {
+      Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> wrapperAction = null;
+      wrapperAction = (arg1,
+        arg2,
+        arg3,
+        arg4,
+        arg5,
+        arg6,
+        arg7,
+        arg8,
+        arg9,
+        arg10,
+        arg11,
+        arg12) =>
+      {
+        source.Value -= wrapperAction;
+        listener(arg1,
+          arg2,
+          arg3,
+          arg4,
+          arg5,
+          arg6,
+          arg7,
+          arg8,
+          arg9,
+          arg10,
+          arg11,
+          arg12);
+      };
+      source.Value += wrapperAction;
+      return source;
+    }
+
+    /// <inheritdoc cref="Once{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}(Reference{Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}}, Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16})"/>
+    public static Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>> Once<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>(
+      this Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>> source,
+      Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> listener)
+    {
+      Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> wrapperAction = null;
+      wrapperAction = (arg1,
+        arg2,
+        arg3,
+        arg4,
+        arg5,
+        arg6,
+        arg7,
+        arg8,
+        arg9,
+        arg10,
+        arg11,
+        arg12,
+        arg13) =>
+      {
+        source.Value -= wrapperAction;
+        listener(arg1,
+          arg2,
+          arg3,
+          arg4,
+          arg5,
+          arg6,
+          arg7,
+          arg8,
+          arg9,
+          arg10,
+          arg11,
+          arg12,
+          arg13);
+      };
+      source.Value += wrapperAction;
+      return source;
+    }
+
+    /// <inheritdoc cref="Once{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}(Reference{Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}}, Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16})"/>
+    public static Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>> Once<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>(
+      this Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>> source,
+      Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> listener)
+    {
+      Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> wrapperAction = null;
+      wrapperAction = (arg1,
+        arg2,
+        arg3,
+        arg4,
+        arg5,
+        arg6,
+        arg7,
+        arg8,
+        arg9,
+        arg10,
+        arg11,
+        arg12,
+        arg13,
+        arg14) =>
+      {
+        source.Value -= wrapperAction;
+        listener(arg1,
+          arg2,
+          arg3,
+          arg4,
+          arg5,
+          arg6,
+          arg7,
+          arg8,
+          arg9,
+          arg10,
+          arg11,
+          arg12,
+          arg13,
+          arg14);
+      };
+      source.Value += wrapperAction;
+      return source;
+    }
+
+    /// <inheritdoc cref="Once{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}(Reference{Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16}}, Action{T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16})"/>
+    public static Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>> Once<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>(
+      this Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>> source,
+      Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> listener)
+    {
+      Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> wrapperAction = null;
+      wrapperAction = (arg1,
+        arg2,
+        arg3,
+        arg4,
+        arg5,
+        arg6,
+        arg7,
+        arg8,
+        arg9,
+        arg10,
+        arg11,
+        arg12,
+        arg13,
+        arg14,
+        arg15) =>
+      {
+        source.Value -= wrapperAction;
+        listener(arg1,
+          arg2,
+          arg3,
+          arg4,
+          arg5,
+          arg6,
+          arg7,
+          arg8,
+          arg9,
+          arg10,
+          arg11,
+          arg12,
+          arg13,
+          arg14,
+          arg15);
+      };
+      source.Value += wrapperAction;
+      return source;
+    }
+
+    /// <summary>
+    /// Registers a specified method to be executed only once on the source's
+    /// referenced delegate.
+    /// </summary>
+    /// <typeparam name="T1">The type of the first parameter of the method
+    /// that will be registered.</typeparam>
+    /// <typeparam name="T2">The type of the second parameter of the method
+    /// that will be registered.</typeparam>
+    /// <typeparam name="T3">The type of the third parameter of the method
+    /// that will be registered.</typeparam>
+    /// <typeparam name="T4">The type of the fourth parameter of the method
+    /// that will be registered.</typeparam>
+    /// <typeparam name="T5">The type of the fifth parameter of the method
+    /// that will be registered.</typeparam>
+    /// <typeparam name="T6">The type of the sixth parameter of the method
+    /// that will be registered.</typeparam>
+    /// <typeparam name="T7">The type of the seventh parameter of the method
+    /// that will be registered.</typeparam>
+    /// <typeparam name="T8">The type of the eighth parameter of the method
+    /// that will be registered.</typeparam>
+    /// <typeparam name="T9">The type of the ninth parameter of the method
+    /// that will be registered.</typeparam>
+    /// <typeparam name="T10">The type of the tenth parameter of the method
+    /// that will be registered.</typeparam>
+    /// <typeparam name="T11">The type of the eleventh parameter of the method
+    /// that will be registered.</typeparam>
+    /// <typeparam name="T12">The type of the twelfth parameter of the method
+    /// that will be registered.</typeparam>
+    /// <typeparam name="T13">The type of the thirteenth parameter of the method
+    /// that will be registered.</typeparam>
+    /// <typeparam name="T14">The type of the fourteenth parameter of the method
+    /// that will be registered.</typeparam>
+    /// <typeparam name="T15">The type of the fifteenth parameter of the method
+    /// that will be registered.</typeparam>
+    /// <typeparam name="T16">The type of the sixteenth parameter of the method
+    /// that will be registered.</typeparam>
+    /// <param name="source">The source encapsulated delegate.</param>
+    /// <param name="listener">The method that will execute only once upon
+    /// registering.</param>
+    /// <returns>The source encapsulated delegate.</returns>
+    public static Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>> Once<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>(
+      this Reference<Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>> source,
+      Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> listener)
+    {
+      Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> wrapperAction = null;
+      wrapperAction = (arg1,
+        arg2,
+        arg3,
+        arg4,
+        arg5,
+        arg6,
+        arg7,
+        arg8,
+        arg9,
+        arg10,
+        arg11,
+        arg12,
+        arg13,
+        arg14,
+        arg15,
+        arg16) =>
+      {
+        source.Value -= wrapperAction;
+        listener(arg1,
+          arg2,
+          arg3,
+          arg4,
+          arg5,
+          arg6,
+          arg7,
+          arg8,
+          arg9,
+          arg10,
+          arg11,
+          arg12,
+          arg13,
+          arg14,
+          arg15,
+          arg16);
+      };
+      source.Value += wrapperAction;
+      return source;
+    }
+  }
+}
